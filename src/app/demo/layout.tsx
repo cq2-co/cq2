bb0dd092b3346778953a20ec3592936f95a37388
anandbaburajan <anandbaburajan@gmail.com>
2024-05-03 13:41:14 +0530

    chore: add favicons for light & dark mode in demo

--- src/app/demo/layout.tsx
+++ src/app/demo/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import lightFavicon from "../../public/logos/cq2-rect-black-transparent.svg";
-import darkFavicon from "../../public/logos/cq2-rect-white-transparent.svg";
+import lightFavicon from "../../../public/logos/cq2-rect-black-transparent.svg";
+import darkFavicon from "../../../public/logos/cq2-rect-white-transparent.svg";
 
 export const metadata: Metadata = {
   title: "CQ2 — demo",
